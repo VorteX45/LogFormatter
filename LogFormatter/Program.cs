076935df387d00695141ad1022ed80da30491fee
VorteX <VorteX@DESKTOP-UKS4OO9>
2026-06-21 11:02:22 +0300

    Добавьте файлы проекта.

---
+++ LogFormatter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LogFormatter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
